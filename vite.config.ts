378f40fe0b1987513efe7d9759228b75536f840f
ByFabrii <fabrizzio.ddd@gmail.com>
2024-12-29 04:32:46 -0600

    git actions to publish to production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // o la ruta específica si no está en la raíz
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
